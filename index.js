15a7ba42723330bc65b2f75a41f1b010587d667c
Christian-Fjord <fjord.christian@gmail.com>
2019-05-18 15:13:34 -0500

    FINALLY GOT THE PORT TO WORK

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code away!
+
+const server = require('./server.js');
+
+server.listen(9393, () => {
+  console.log('Server running on port 9393');
+});
--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,21 +1,84 @@
-const express = 'express';
+const express = require('express');
+
+const dbPosts = require("./postDb.js");
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+  dbPost
+    .get()
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((err) => {
+      res.status(500).json({err: "The post information could not be retrieved."});
+    })
 });
 
 router.get('/:id', (req, res) => {
+  dbPosts
+    .getById(req.params.id)
+    .then((post) => {
+      if (post.length === 0) {
+        res.status(404).json({message: "The post with this ID does not exist"});
+      } else {
+        res.status(200).json(post);
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({error: "The post information could not be retrieved"});
+    })
+});
 
+router.post('/', (req, res) => {
+  const newPost = req.body;
+  if (!newPost.hasOwnProperty("title") || !newPost.hasOwnProperty("contents")) {
+    res.status(400).json({ errorMessage: "Please provide title and contents for the post"});
+  }
+  dbPosts
+    .insert(newPost)
+    .then((idOfNewPost) => {
+      res.status(201).json(idOfNewPost);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "There was an error while saving the post to the database"});
+  });
 });
 
 router.delete('/:id', (req, res) => {
-
+  dbPosts
+    .remove(req.params.id)
+    .then((numofPostDeleted) => {
+      if (!numofPostDeleted) {
+        res.status(404).json({message: "The post with this ID does not exist"});
+      } else {
+        res.status(204).end;
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({error: "The post was couldn't be removed"});
+    })
 });
 
 router.put('/:id', (req, res) => {
-
+  const postToUpdate = req.body;
+  if (!postToUpdate.hasOwnProperty("title") || !postToUpdate.hasOwnProperty("contents")) {
+    res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+  }
+  dbPosts
+    .update(req.params.id, postToUpdate)
+    .then((numOfUpdatedPosts) => {
+      if (!numOfUpdatedPosts) {
+        res.status(404).json({ message: "The post with the specified ID does not exist" });
+      } else {
+        dbPosts.getById(req.params.id).then((updatedPost) => {
+          res.status(200),json(updatedPost);
+        })
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "The post information could not be modified." })
+    })
 });
 
 // custom middleware
